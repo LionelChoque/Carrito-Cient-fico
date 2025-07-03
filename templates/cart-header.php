e7458a9a9fbbbf31925a1b43bec0d5b765890127
Cuidandoelagua517 <dgil@bairesanalitica.com>
2025-07-03 00:11:10 -0300

    Create cart-header.php

---
+++ templates/cart-header.php
@@ -0,0 +1,12 @@
+?>
+<?php if (!defined('ABSPATH')) exit; ?>
+
+<div class="scientific-cart-header">
+    <div class="scientific-icon">
+        <?php echo $icon; ?>
+    </div>
+    <h2 class="scientific-title"><?php echo esc_html($title); ?></h2>
+    <p class="scientific-subtitle"><?php echo esc_html($subtitle); ?></p>
+</div>
+
+<?php
